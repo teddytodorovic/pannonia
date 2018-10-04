842546eeb5355bc2e2eeeb30fc998c5a795d04ad
Ted <t_todorovic@hotmail.com>
2018-10-04 12:39:40 -0500

    filled out IDateTime interface

--- DateTimeCustom/DateTimeCustom/IDateTime.cs
+++ DateTimeCustom/DateTimeCustom/IDateTime.cs
@@ -6,6 +6,13 @@ namespace DateTimeCustom
 {
     public interface IDateTime
     {
-        public DateTime DateTime { get; set; }
+        DateTime Now { get; }
+        DateTime Today { get; }
+        DateTime DateTime { get; set; }
+        DateTime AddDays(double i);
+        DayOfWeek DayOfWeek { get; }
+        int Day { get; }
+        int CompareTo(IDateTime startTransferDate);
+        string ToShortDateString();
     }
 }
