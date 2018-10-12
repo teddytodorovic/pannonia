6f38b29783f45f02edf9edea4bcb8ccb189c97ec
Ted <t_todorovic@hotmail.com>
2018-10-12 17:52:33 -0500

    New CrackerJack folder containing useful programming utilities.

---
+++ CrackerJack/PartialClassExtension/PartialClassExtension/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PartialClassExtension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
